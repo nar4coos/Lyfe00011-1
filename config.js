321344eb4a9c88ff5911424cbf87952b1f4c4534
Neeraj <86142113+neera-j@users.noreply.github.com>
2022-05-17 22:08:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const DATABASE_URL =
 		: process.env.DATABASE_URL
 module.exports = {
 	VERSION: '2.2.7',
-	SESSION_ID: process.env.SESSION_ID || '',
+	SESSION_ID:'5f93ca_7b93_17',
 	DATABASE:
 		DATABASE_URL === './database.db'
 			? new Sequelize({
